28c2e718fe4a42eecdbed3a5c6cd08a2807cfbe8
leticianb <leticianakahara10@gmail.com>
2021-06-05 17:46:13 -0300

    a

--- src/eixo/controler/neweixo.js
+++ src/eixo/controler/neweixo.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
     $('.btn-new').click(function(e) {
         e.preventDefault()
+
+        $('.modal-title').empty()
+        $('.modal-body').empty()
+
+        $('.modal-title').append('Adicionar novo eixo tecnológico')
+
         $('#modaleixo').modal('show')
     })
 })
