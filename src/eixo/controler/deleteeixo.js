83d47af5d4ee7e1c866402595ada8bcc56dbd80b
leticianb <leticianakahara10@gmail.com>
2021-06-07 23:38:16 -0300

    a

---
+++ src/eixo/controler/deleteeixo.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+    $('#table-eixo').on('click', 'button.btn-delete', function(e) {
+
+
+        e.preventDefault()
+
+        let ideixo = `ideixo=${$(this).attr('id')}`
+
+        Swal.fire({
+            title: 'Library',
+            text: 'Deseja realmente excluir esse registro?',
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonText: 'Sim',
+            cancelButtonText: 'Não',
+
+        }).then((result => {
+            if (result.value) {
+                $.ajax({
+                    type: 'POST',
+                    dataType: 'json',
+                    assync: true,
+                    data: ideixo,
+                    url: 'src/eixo/model/deleteeixo.php',
+                    success: function(dados) {
+                        Swal.fire({
+                            title: 'Library',
+                            text: dado.mensagem,
+                            type: dado.tipo,
+                            confirmButtonText: 'OK'
+                        })
+
+                        $('#table-eixo'.DataTable().ajax.reload())
+                    }
+                })
+            }
+        }))
+    })
+})
--- src/eixo/model/deleteeixo.php
+++ src/eixo/model/deleteeixo.php
@@ -3,4 +3,20 @@
     
     include('../../conexao/conn.php');
 
-    
+    $ideixo = $_REQUEST['ideixo'];
+    $sql = "DELETE FROM eixo WHERE ideixo = $ideixo";
+    $resultado = $pdo -> query($sql);
+    if($resultado){
+        $dados = array(
+            'tipo' => 'success'
+            'mensagem' =>'Registro excluído com sucesso'
+        );
+    }
+    else{
+        $dados = array(
+            'tipo' => 'error'
+            'mensagem' =>'Não foi possível excluir o registro'
+        );
+    }
+
+    echo json_encode($dados);
