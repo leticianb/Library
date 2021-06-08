ba79f994d1251ca1f9f38d4b2b82164e15320471
leticianb <leticianakahara10@gmail.com>
2021-06-07 23:03:30 -0300

    a

---
+++ src/eixo/controler/editeixo.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+    $('#table-eixo').on('click', 'button.btn-edit', function(e) {
+
+        e.preventDefault()
+
+
+
+        $('.modal-title').empty()
+        $('.modal-body').empty()
+
+        $('.modal-title').append('Edição de eixo tecnológico')
+
+        let ideixo = `ideixo=${$(this).attr('id')}`
+
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            assync: true,
+            data: ideixo,
+            url: 'src/eixo/model/vieweixo.php',
+            success: function(dado) {
+                if (dado.tipo == "success") {
+                    $('.modal-body').load('src/eixo/view/formeixo.html', function() {
+                        $('#nome').val(dado.dados.nome)
+                        $('#ideixo').val(dado.dados.ideixo)
+                    })
+                    $('.btn-save').shows()
+                    $('#modaleixo').modal('show')
+                } else {
+                    Swal.fire({
+                        title: 'Library',
+                        text: dado.mensagem,
+                        type: dado.tipo,
+                        confirmButtonText: 'OK'
+                    })
+                }
+            }
+        })
+
+    })
+})
