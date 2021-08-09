732604b882455f9b979beac20a5b3bb8c77aa4dc
leticianb <leticianakahara10@gmail.com>
2021-08-09 19:32:19 -0300

    a

--- src/curso/controler/newcurso.js
+++ src/curso/controler/newcurso.js
@@ -6,7 +6,19 @@ $(document).ready(function() {
         $('.modal-body').empty()
 
         $('.modal-title').append('Adicionar novo curso')
-        $('.modal-body').load('src/curso/view/formcurso.html')
+        $('.modal-body').load('src/curso/view/formcurso.html', function() {
+            $.ajax({
+                type: 'POST',
+                dataType: 'json',
+                assync: true,
+                url: 'src/eixo/model/alleixo.php',
+                success: function(dados) {
+                    for (const dado of dados) {
+                        $('#eixo_ideixo').append(`<option value="${dado.ideixo}">${dado.nome}</option>`)
+                    }
+                }
+            })
+        })
         $('.btn-save').show()
         $('.btn-save').attr('data-operation', 'insert')
         $('#modalcurso').modal('show')
