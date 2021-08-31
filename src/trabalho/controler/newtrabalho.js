fa538c162cafd711d87f52bc12afbe924462a032
leticianb <leticianakahara10@gmail.com>
2021-08-30 21:29:24 -0300

    a

---
+++ src/trabalho/controler/newtrabalho.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('.btn-new').click(function(e) {
+        e.preventDefault()
+
+        $('.modal-title').empty()
+        $('.modal-body').empty()
+
+        $('.modal-title').append('Adicionar novo trabalho')
+
+        $('.modal-body').load('src/trabalho/view/formtrabalho.html', function() {
+
+        })
+
+        $('.btn-save').show()
+
+        $('.btn-save').attr('data-operation', 'insert')
+
+        $('#modaltrabalho').modal('show')
+    })
+})
